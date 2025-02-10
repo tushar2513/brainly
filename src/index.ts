db482ee27145c9ae2bc0d49dbb0ec03a851198e9
tushar <tusharpol825@gmail.com>
2025-02-10 17:56:00 +0530

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import mongoose from "mongoose"; 
+import jwt from "jsonwebtoken"; 
+const app = express(); 
+
+app.post("/signup", (req, res) =>{
+
+})
+
+app.post("/signin", (req, res) =>{
+    
+})
+
+app.post("/content", (req, res) =>{
+    
+})
+
+app.get("/content", (req, res) =>{
+    
+})
+
+app.delete("/delete", (req, res) =>{
+    
+})
+
+app.post("/brain/share", (req, res) =>{
+    
+})
+
+app.get("/brain/:sharelink", (req, res) =>{
+    
+})
+app.listen(3000)
